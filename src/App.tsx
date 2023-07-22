ec0b76b578b5064ec9faf13831596a4b2455d663
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-21 20:17:58 -0400

    Setup Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import UserPage from './pages/users/UserPage';
 import ProductPage from './pages/products/ProductPage';
+import Navbar from './components/navbar/Navbar';
+import Menu from './components/menu/Menu';
 
 function App() {
   const Layout = () => {
-    return <div className="main"></div>;
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menu-container">
+            <Menu />
+          </div>
+          <div className="content-container">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    );
   };
 
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Homepage />,
-    },
-    {
-      path: 'users',
-      element: <UserPage />,
-    },
-    {
-      path: 'products',
-      element: <ProductPage />,
+      element: <Layout />,
+      chlidren: [
+        {
+          path: '/',
+          element: <Homepage />,
+        },
+        {
+          path: '/users',
+          element: <UserPage />,
+        },
+        {
+          path: '/products',
+          element: <ProductPage />,
+        },
+      ],
     },
   ]);
 
