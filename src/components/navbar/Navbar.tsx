a92e11abd4d99d482cd418de7253c209608f9ab5
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-22 13:20:57 -0400

    working on Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,23 @@
 import './Navbar.scss';
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="" alt="" className="logo-image" />
+        <span>Amrindra</span>
+      </div>
+
+      <div className="icons">
+        <img src="/searchIcon.svg" alt="" className="icon" />
+        <img src="" alt="" className="icon" />
+        <img src="" alt="" className="icon" />
+        <div className="notification"></div>
+        <div className="user"></div>
+        <img src="" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
