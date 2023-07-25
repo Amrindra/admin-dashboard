a0292a0e5fc7b1743eb0d1af77a029e9c5b52ad1
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-25 15:56:40 -0400

    Completed the footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import './Footer.scss';
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <span>Amrin</span>
+      <span>Amrin's Admin dashboard</span>
+    </footer>
+  );
 };
 
 export default Footer;
