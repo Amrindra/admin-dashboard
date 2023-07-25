5204ed381551d7494cc3f8846c172cf218f689d4
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-25 16:12:04 -0400

    Working on Menu bar

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,34 @@
 import './Menu.scss';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="list-item">
+          <img src="/homeIcon.svg" alt="homepage Icon" />
+          <span className="list-item-title">Home</span>
+        </Link>
+        <Link to="/" className="list-item">
+          <img src="/profileIcon.svg" alt="homepage Icon" />
+          <span className="list-item-title">Profile</span>
+        </Link>
+      </div>
+
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="list-item">
+          <img src="/homeIcon.svg" alt="homepage Icon" />
+          <span className="list-item-title">Home</span>
+        </Link>
+        <Link to="/" className="list-item">
+          <img src="/profileIcon.svg" alt="homepage Icon" />
+          <span className="list-item-title">Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
