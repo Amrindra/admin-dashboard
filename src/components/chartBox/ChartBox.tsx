8bed7af87dbff12b135606ab2d2811fc9771b569
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-26 15:16:38 -0400

    2 files modified

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,7 +1,62 @@
 import { Link } from 'react-router-dom';
 import './ChartBox.scss';
+import React, { PureComponent } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const ChartBox = () => {
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <div className="chart-box-container">
       <div className="box-info">
@@ -12,9 +67,24 @@ const ChartBox = () => {
         <h3>11.323</h3>
         <Link to="/">View All</Link>
       </div>
+
       <div className="chart-info">
-        <div className="chart"></div>
-        <div className="texts"></div>
+        <div className="chart">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={300} height={100} data={data}>
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                strokeWidth={2}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <div className="percentage">45%</div>
+          <div className="duration">This month</div>
+        </div>
       </div>
     </div>
   );
