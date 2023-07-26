1ac034751f375e4e38d1a59f417db1db1262beb0
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-26 10:50:06 -0400

    Created chart component and installed Rechart libray

---
+++ src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,11 @@
+import './ChartBox.scss';
+
+const ChartBox = () => {
+  return (
+    <div className="chart-box-container">
+      <h3>chartbox</h3>
+    </div>
+  );
+};
+
+export default ChartBox;
