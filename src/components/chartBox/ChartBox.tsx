b0dc3696327544db34ca046da83d77ebc43c85f7
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-26 13:43:27 -0400

    1 file modified

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
 import './ChartBox.scss';
 
 const ChartBox = () => {
   return (
     <div className="chart-box-container">
-      <h3>chartbox</h3>
+      <div className="box-info">
+        <div className="title-section">
+          <img src="/userIcon.svg" alt="" className="" />
+          <span>Total Users</span>
+        </div>
+        <h3>11.323</h3>
+        <Link to="/">View All</Link>
+      </div>
+      <div className="chart-info">
+        <div className="chart"></div>
+        <div className="texts"></div>
+      </div>
     </div>
   );
 };
