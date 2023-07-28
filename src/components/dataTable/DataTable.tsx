0ea8b64041aad13c66d9d0d9c6e2c92bd38f75c8
Amrindra <70451928+Amrindra@users.noreply.github.com>
2023-07-27 21:08:14 -0400

    2 files modified

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -10,6 +10,14 @@ const DataTable = () => {
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
+      field: 'avatar',
+      headerName: 'Avatar',
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || 'userIcon.svg'} alt="" />;
+      },
+    },
+    {
       field: 'firstName',
       headerName: 'First name',
       width: 150,
