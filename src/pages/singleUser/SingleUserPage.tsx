e698aadaaf21c3d8cdb396378fdc957b8bc97c83
Amrindra Ouk <amrindraouk@yahoo.com>
2023-07-31 11:09:58 -0400

    Working on singlepage user

--- src/pages/singleUser/SingleUserPage.tsx
+++ src/pages/singleUser/SingleUserPage.tsx
@@ -1,7 +1,13 @@
+import Single from "../../components/single/Single";
+import { singleUser } from "../../utils/data";
 import "./SingleUserPage.scss";
 
 const SingleUserPage = () => {
-  return <div className="single-user-page">SingleUserPage</div>;
+  return (
+    <div className="single-user-page">
+      <Single {...singleUser} />
+    </div>
+  );
 };
 
 export default SingleUserPage;
